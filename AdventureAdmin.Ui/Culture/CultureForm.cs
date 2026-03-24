bc587cad4942fab43b369702295710631ce704d1
Diana <Ashleymanuela19@gmail.com>
2026-03-24 09:53:33 -0400

    Merge with master

---
+++ AdventureAdmin.Ui/Culture/CultureForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdventureAdmin.Ui.Culture
+{
+    public partial class CultureForm : Form
+    {
+        public CultureForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CultureForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
